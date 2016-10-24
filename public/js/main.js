e2284ec45b4a6fe739eb2534e46314a53b516d61
vikaba <vikaba@bergen.org>
2016-10-23 23:02:29 -0400

    comment

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,16 @@
+$(document).ready(function() {
+	getWeather();
+});
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
+function getWeather(){
+	var url = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&id=4930956&APPID=' + apiKey;
+	$.ajax(url, {
+		success: function (data) {
+			$('.city').text(data.name);
+			$('.temp').text(data.main.temp + ' F');
+			$('.humidity').text(data.main.humidity);
+			$('.wind')text(data.wind.speed);
+		}
+	});
 }
 
