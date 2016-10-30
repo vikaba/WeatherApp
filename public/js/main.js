a936190feab1d360a4be498a78afbd8413e7344c
vikaba <vikaba@bergen.org>
2016-10-30 01:40:14 -0400

    comment

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ function searchWeather() {
 }
 
 function getWeather(searchQuery) {
-	var url = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&id=4930956&APPID=' + apiKey;
+	var url = 'http://api.openweathermap.org/data/2.5/weather?';
 	var params = {
     APPID: apiKey,
     units: 'imperial'
